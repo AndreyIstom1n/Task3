f4fe2816024a6cb36bc26ca3644dc109d5e56725
AndreyIstom1n <113121599+AndreyIstom1n@users.noreply.github.com>
2023-03-29 21:46:23 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -3,7 +3,8 @@
   <p><input type="text" name="fio" placeholder="Ваше имя"></p>
 
   <p><input type="email" name="email" placeholder="Ваша почта"></p>
-  <br>Ваш год рождения
+  <br>
+  <h2>Ваш год рождения</h2>
   <select name="year">
     <?php 
     for ($i = 1900; $i <= 2023; $i++) {
@@ -12,26 +13,31 @@
     ?>
   </select>
   <br>
-  <br>Ваш пол
+  <br>
+  <h2>Ваш пол</h2>
   <p><input type="radio" name="r1[]" value="male"> Мужской</p>
   <p><input type="radio" name="r1[]" value="female"> Женский</p>
   <p><input type="radio" name="r1[]" value="other">Другой</p>
 
-  <br> Количество ваших конечностей
+  <br> 
+  <h2>Количество ваших конечностей</h2>
   <p><input type="radio" name="r2[]" value="2">2</p>
   <p><input type="radio" name="r2[]" value="3">3</p>
   <p><input type="radio" name="r2[]" value="4">4</p>
   <p><input type="radio" name="r2[]" value="many">4+</p>
-  <br> Ваши сверхспособности
+  <br> 
+  <h2>Ваши сверхспособности</h2>
   <p><select multiple="multiple" name="abilities[]">
     <option value="Immortality">Бессмертие</option>
     <option value="Passing through walls">Прохождение сквозь стены</option>
     <option value="Levitation">Левитация</option>
     </select>
   </p>
-  <br>Ваша биография
+  <br>
+  <h2>Ваша биография</h2>
   <p><textarea name="biography"></textarea></p>
-  <br>С контрактом ознакомлен(а)
+  <br>
+  <h2>С контрактом ознакомлен(а)</h2>
   <p><input type="checkbox" name="cb"></p>
   <input type="submit" value="Подтверждаю" />
 </form>
